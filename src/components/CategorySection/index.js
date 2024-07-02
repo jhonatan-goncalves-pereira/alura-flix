998d8a6b2a770cd375771fe183ae42a5a9a44fba
Jhonatan <jhonatangoncalves2002@gmail.com>
2024-07-02 16:42:39 -0300

    JS: Ajustando a requisição DELETE para exclusão de card

--- src/components/CategorySection/index.js
+++ src/components/CategorySection/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './CategorySection.module.css';
 
-const CategorySection = ({ title, videos = [], onEdit, onDelete }) => (
+const CategorySection = ({ title, videos, onEdit, onDelete }) => (
   <section className={styles.categorySection}>
     <h3>{title}</h3>
     <div className={styles.videos}>
-      {videos.map((video) => (
-        <div key={video.id} className={styles.videoItem}>
+      {videos.map((video, index) => (
+        <div key={index} className={styles.videoItem}>
           <img src={video.imageUrl} alt={video.title} />
           <p>{video.title}</p>
-          <div className={styles.buttonContainer}>
+          <div className={styles.buttons}>
             <button onClick={() => onEdit(video)}>Editar</button>
-            <button onClick={() => onDelete(title.toLowerCase(), video.id)}>Excluir</button>
+            <button onClick={() => onDelete(video.id)}>Excluir</button>
           </div>
         </div>
       ))}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,5 @@
+// Home.js
+
 import React, { useState, useEffect } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -30,33 +32,37 @@ const Home = () => {
     setEditModalOpen(true);
   };
 
+  const handleDelete = (id) => {
+    fetch(`http://localhost:3001/videos/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log(`Video with ID ${id} deleted successfully.`);
+          // Atualiza localmente removendo o vídeo da lista
+          const updatedVideos = videos.filter(video => video.id !== id);
+          setVideos(updatedVideos);
+        } else {
+          console.error(`Failed to delete video with ID ${id}.`);
+        }
+      })
+      .catch(error => {
+        console.error('Error deleting video:', error);
+      });
+  };
+
   const handleCloseModal = () => {
     setEditModalOpen(false);
     setSelectedVideo(null);
   };
 
   const handleSaveModal = (updatedVideo) => {
-    fetch(`http://localhost:3001/videos/${updatedVideo.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedVideo),
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Video updated successfully:', data);
-        // Atualiza o vídeo localmente após edição
-        const updatedVideos = videos.map(video =>
-          video.id === updatedVideo.id ? updatedVideo : video
-        );
-        setVideos(updatedVideos);
-        handleCloseModal(); // Fecha o modal após salvar
-      })
-      .catch(error => {
-        console.error('Error updating video:', error);
-        // Trate o erro adequadamente, se necessário
-      });
+    // Atualiza o vídeo localmente após edição
+    const updatedVideos = videos.map(video =>
+      video.id === updatedVideo.id ? updatedVideo : video
+    );
+    setVideos(updatedVideos);
+    handleCloseModal(); // Fecha o modal após salvar
   };
 
   return (
@@ -64,10 +70,30 @@ const Home = () => {
       <Header />
       <main className={styles.home}>
         <h1>AluraFlix</h1>
-        <CategorySection title="Frontend" videos={videos.filter(video => video.category === 'Frontend')} onEdit={handleEdit} />
-        <CategorySection title="Backend" videos={videos.filter(video => video.category === 'Backend')} onEdit={handleEdit} />
-        <CategorySection title="Inovação" videos={videos.filter(video => video.category === 'Inovação')} onEdit={handleEdit} />
-        <CategorySection title="Gestão" videos={videos.filter(video => video.category === 'Gestão')} onEdit={handleEdit} />
+        <CategorySection
+          title="Frontend"
+          videos={videos.filter(video => video.category === 'Frontend')}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+        <CategorySection
+          title="Backend"
+          videos={videos.filter(video => video.category === 'Backend')}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+        <CategorySection
+          title="Inovação"
+          videos={videos.filter(video => video.category === 'Inovação')}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+        <CategorySection
+          title="Gestão"
+          videos={videos.filter(video => video.category === 'Gestão')}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
       </main>
       <Footer />
       {editModalOpen && (
